48b117199c5a2968fb5e7c88664e9de32312b598
jm Lee <jm0365@sen.go.kr>
2020-04-20 20:04:48 +0900

    branchtest파일 추가

---
+++ BranchTest.java
@@ -0,0 +1,11 @@
+public class BranchTest {
+	public static void main( String args[ ] ) {
+	System.out.println("Welcome to JavaWorld") ;
+<<<<<<< HEAD
+
+=======
+	System.out.println("hahaha") ;
+>>>>>>> testbranch
+	System.out.println("lalala") ;
+	}
+}
