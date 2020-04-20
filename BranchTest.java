e274e5c2106f3ecf7a1467ed2119c90865e77548
jm Lee <jm0365@sen.go.kr>
2020-04-20 20:06:55 +0900

    출력문 수정

--- BranchTest.java
+++ BranchTest.java
@@ -1,11 +1,6 @@
 public class BranchTest {
 	public static void main( String args[ ] ) {
 	System.out.println("Welcome to JavaWorld") ;
-<<<<<<< HEAD
-
-=======
 	System.out.println("hahaha") ;
->>>>>>> testbranch
-	System.out.println("lalala") ;
 	}
 }
